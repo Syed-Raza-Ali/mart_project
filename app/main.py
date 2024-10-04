7444d6fefa154a575f41682f4a5f3071038152bb
SYED RAZA ALI <144608213+Syed-Raza-Ali@users.noreply.github.com>
2024-10-04 23:48:44 +0500

    add commit to define code

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from typing import Annotated, Optional
 from fastapi import Depends, FastAPI
 from sqlmodel import Session, Field, SQLModel,create_engine
-
 from app.settings import DATABASE_URL
 
 # create a user base  class for store user data in database 
-
 class UserBase(SQLModel):
     user_name : str
     user_address : str
     user_email : str
     user_password : str
+
+
 
 # create a user class for generate id automatically 
 class User(UserBase, table=True):
@@ -22,7 +22,6 @@ class User(UserBase, table=True):
  
 # .replace() for replace postgresql to postgresql + psycopg for using psycopg in postgrsql
 #connection string for connect data base 
-
 connection_string = str(DATABASE_URL).replace(
     "postgresql", "postgresql+psycopg"
 )
@@ -34,7 +33,6 @@ connection_string = str(DATABASE_URL).replace(
 # pool_recycle=300 = reload after 300 seconds to update connection string
 # Is engine ke through aap database ke saath kaam karte ho. Hum " connection_string " pass karte hain jo bataata hai ke kis database se connect karna hai.
 # connect_args ek parameter hai jo humein database connection ke liye additional options dene ki sahoolat deta hai. Is code mein humne empty dictionary di hai, iska matlab hai koi extra argument nahi diya.
-
 engine = create_engine(
     connection_string, connect_args={}, pool_recycle=300
 )
@@ -43,7 +41,6 @@ engine = create_engine(
 
 # create_db_and_tables Ye function FastAPI ko argument ke tor pe accept karta hai, taake jab app chalaye to yeh function database ke tables create kar sake.
 # SQLModel.metadata.create_all(engine) se SQLModel ka metadata (jo database ke tables ke structure ko define karta hai) ko use karke sab tables ko database mein create kiya jata hai.
-
 async def create_db_and_tables(app: FastAPI):
     print(f"Creating tables...{app}") 
     SQLModel.metadata.create_all(engine)
@@ -62,10 +59,6 @@ def get_session():
 DB_Session = Annotated[Session, Depends(get_session)]
 
 
-
-
-
-
 # add FastAPI to create routes
 app = FastAPI(lifespan= create_db_and_tables) 
 
@@ -74,13 +67,18 @@ app = FastAPI(lifespan= create_db_and_tables)
 def root_route():
     return {"Hello": "From Server"}
 
+
+
 # add user in database 
 def add_user_into_db(form_data : UserBase, session : Session):
     user_info = User(**form_data.dict())
-
+    # Add user to database
     session.add(user_info)
+    # Commit the changes to the database
     session.commit()
+    # Refresh the user object to get the id, this is needed for the next commit operation
     session.refresh(user_info)
+    # print statement for debugging purposes
     print("new user..", user_info)
 
     return user_info
